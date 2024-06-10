37bb68bae801b53427a4d234c30f47fe6837aeed
Ville Heikkilä <ville.heikkila@tuni.fi>
2024-06-10 17:25:54 +0300

    Fix issues with SSL configurations

--- demo_platform/background/utility_scripts/mongodb-init.sh
+++ demo_platform/background/utility_scripts/mongodb-init.sh
@@ -1,2 +1,2 @@
-mongo --tls $MONGODB_HOST:$MONGODB_PORT/admin mongodb-init-admin.js
-mongo --tls $MONGODB_HOST:$MONGODB_PORT/$MONGO_INITDB_DATABASE mongodb-init-user.js
+mongo --tls $MONGODB_HOST:$MONGODB_PORT/admin --sslAllowInvalidCertificates mongodb-init-admin.js
+mongo --tls $MONGODB_HOST:$MONGODB_PORT/$MONGO_INITDB_DATABASE --sslAllowInvalidCertificates mongodb-init-user.js
